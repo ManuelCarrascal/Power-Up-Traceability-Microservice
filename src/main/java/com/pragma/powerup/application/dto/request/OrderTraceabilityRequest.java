9f1a60338004a8e93611b24930ba5552d872e4cf
Manuel Carrascal <tricdiart@gmail.com>
2025-03-26 16:46:24 -0500

    feat: add open api documentation

--- src/main/java/com/pragma/powerup/application/dto/request/OrderTraceabilityRequest.java
+++ src/main/java/com/pragma/powerup/application/dto/request/OrderTraceabilityRequest.java
@@ -2,6 +2,7 @@ package com.pragma.powerup.application.dto.request;
 
 import com.pragma.powerup.domain.model.OrderDish;
 import com.pragma.powerup.domain.model.StatusChange;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,18 +14,24 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Request object for creating or updating order traceability information")
 public class OrderTraceabilityRequest {
 
+    @Schema(description = "Unique identifier for the order", example = "1001")
     private Long id;
 
+    @Schema(description = "Client's unique identifier", example = "5001", required = true)
     private Long idClient;
 
+    @Schema(description = "Employee's unique identifier who processed the order", example = "3001")
     private Long idEmployee;
 
+    @Schema(description = "Restaurant's unique identifier", example = "2001", required = true)
     private Long idRestaurant;
 
+    @Schema(description = "List of dishes included in the order", required = true)
     private List<OrderDish> dishes;
 
+    @Schema(description = "History of status changes for the order")
     private List<StatusChange> statusChanges;
-
 }
--- src/main/java/com/pragma/powerup/application/dto/response/OrderTraceabilityResponse.java
+++ src/main/java/com/pragma/powerup/application/dto/response/OrderTraceabilityResponse.java
@@ -1,6 +1,7 @@
 package com.pragma.powerup.application.dto.response;
 
 import com.pragma.powerup.domain.model.StatusChange;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +13,15 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Response object containing order traceability information")
 public class OrderTraceabilityResponse {
 
+    @Schema(description = "Unique identifier for the order", example = "60f1a5c2e8d7b318945c9c23")
     private String id;
 
+    @Schema(description = "Restaurant's unique identifier", example = "2001")
     private Long idRestaurant;
 
+    @Schema(description = "History of status changes for the order")
     private List<StatusChange> statusChanges;
 }
